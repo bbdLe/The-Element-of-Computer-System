93e55edc898fb9252cedf2c8dc45c20f31ae40e2
bbdLe <dalibaxiaoliba19@gmail.com>
2017-10-11 16:39:45 +0800

    No finish

--- 10/Compiler/Compiler/Program.cs
+++ 10/Compiler/Compiler/Program.cs
@@ -10,6 +10,19 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("Usage: program filename");
+                Environment.Exit(-1);
+            }
+            Console.WriteLine(args);
+            Tokenizer tokenizer = new Tokenizer(args[0]);
+            while(tokenizer.hasMoreTokens())
+            {
+                var tokenType = tokenizer.tokenType();
+                Console.WriteLine(tokenType);
+                tokenizer.advance();
+            }
         }
     }
 }
--- 10/Compiler/Compiler/Tokenizer.cs
+++ 10/Compiler/Compiler/Tokenizer.cs
@@ -9,63 +9,175 @@ namespace Compiler
         public Tokenizer(string fileName)
         {
             strs = File.ReadAllText(fileName);
-
-
         }
 
         public bool hasMoreTokens()
         {
-            return curr_count >= tokens.Length? false : true;
+            skitWhiteSpace();
+            skitComment();
+
+            return curr_count >= strs.Length? false : true;
         }
 
         public void advance()
         {
+            string str = keyWord();
+            if(str != string.Empty)
+            {
+                mKeyword = str;
+                mTokenType = "KeyWord";
+                return;
+            }
 
+            str = findSymbol();
+            if(str != string.Empty)
+            {
+                mSymbol = str;
+                mTokenType = "Symbol";
+                return;
+            }
+
+            str = intNumber();
+            if(str != string.Empty)
+            {
+                mInt = str;
+                mTokenType = "Int";
+            }
+
+            str = constString();
+            if(str != string.Empty)
+            {
+                mString = str;
+                mTokenType = "String";
+            }
+
+            str = identify();
+            if(str != string.Empty)
+            {
+                midentifier = str;
+                mTokenType = "Identify";
+            }
+
+            Console.WriteLine("Bad Result");
+            Environment.Exit(-1);
+        }
+
+        public string identifier()
+        {
+            return midentifier;
+        }
+
+        public string symbol()
+        {
+            return mSymbol;
+        }
+
+        public string stringVal()
+        {
+            return mString;
+        }
+
+        public string intVal()
+        {
+            return mInt;
+        }
+
+        public string tokenType()
+        {
+            return mTokenType;
         }
 
         private string singleLineComment()
         {
-            var match = singleLineCommentRegex.Match(strs, curr_count);
-
-            if (match.Success == false)
-                return string.Empty;
-            else
-            {
-                return strs.Substring(match.Index, match.Length);
-            }
+            return regexTemplate(singleLineCommentRegex);
         }
 
         private string multiLineComment()
         {
-            var match = multiLineCommentRegex.Match(strs, curr_count);
-
-            if (match.Success == false)
-                return string.Empty;
-            else
-            {
-                return strs.Substring(match.Index, match.Length);
-            }
+            return regexTemplate(multiLineCommentRegex);
         }
 
-        private string whitSpace()
+        private string keyWord()
         {
-            var match = multiLineCommentRegex.Match(strs, curr_count);
+            return regexTemplate(keyWordRegex);
+        }
 
-            if (match.Success == false)
-                return string.Empty;
-            else
+        private string identify()
+        {
+            return regexTemplate(identifyRegex);
+        }
+
+        private string constString()
+        {
+            return regexTemplate(stringRegex);
+        }
+
+        private string intNumber()
+        {
+            return regexTemplate(intRegex);
+        }
+
+        private string findSymbol()
+        {
+            return regexTemplate(symbolRegex);
+        }
+
+
+        private string regexTemplate(Regex regex)
+        {
+            var match = regex.Match(strs, curr_count);
+
+            if(match.Success == true && match.Index == curr_count)
             {
                 return strs.Substring(match.Index, match.Length);
             }
+            else
+            {
+                return string.Empty;
+            }
         }
 
-        private string 
+        private void skitWhiteSpace()
+        {
+            var match = whiteSpaceRegex.Match(strs, curr_count);
+
+            if (match.Success == true && match.Index == curr_count)
+                curr_count += match.Length;
+        }
+
+        private void skitComment()
+        {
+            var str = singleLineComment();
+            curr_count += str.Length;
+            str = multiLineComment();
+            curr_count += str.Length;
+        }
 
         private Regex singleLineCommentRegex = new Regex(@"//.*");
+        private Regex keyWordRegex = new Regex(@"class\s+|method\s+|int\s+|
+                                                 function\s+|boolean\s+|
+                                                 constructor\s+|char\s+|
+                                                 void\s+|var\s+|static\s+|
+                                                 field\s+|let\s+|do\s+|
+                                                 if\s+|else\s+|while\s+|
+                                                 return\s+|true\s+|false\s+|
+                                                 null\s+|this\s+");
+        private Regex symbolRegex = new Regex(@"\{|\}|\(|\)|\[|\]|\.|,|;|\+|-|\*|/|&|\||<|>|=|~");
+        private Regex identifyRegex = new Regex(@"[a-zA-Z_]\w*");
+        private Regex stringRegex = new Regex(@""".+""");
         private Regex multiLineCommentRegex = new Regex(@"/\*.*\*/");
-        private Regex whiteSpaceRegex = new Regex(@"\s");
+        private Regex intRegex = new Regex(@"\d*");
+        private Regex whiteSpaceRegex = new Regex(@"\s+");
+
         private string strs;
         private string tokens;
         private int curr_count;
+
+        private string mSymbol;
+        private string midentifier;
+        private string mInt;
+        private string mString;
+        private string mKeyword;
+        private string mTokenType;
     }
 }
